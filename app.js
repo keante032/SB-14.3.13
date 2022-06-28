624bc6cbcee135c1345983ec77b5d41ee7ef30a2
Kevin Templeton <templeton.kevin@gmail.com>
2022-06-27 22:09:36 -0500

    Part 2: Appending GIFs

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ $(function () {
   $("#searchForm").on("submit", async function (e) {
     e.preventDefault();
     let query = $("#searchText").val();
-    let response = await axios.get(
-      `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
-    );
-    console.log("got", response);
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+      params: {
+        q: query,
+        api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym",
+      },
+    });
+    let rand50 = Math.floor(Math.random() * 50);
+    let newGif = $("<img>", {
+      src: response.data.data[rand50].images.downsized.url,
+    });
+    $("#gifContainer").append(newGif);
   });
 });
